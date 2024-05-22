42881f807230c2b87f7eef36f18d4be962ddb320
Brandon Wie <brandonwie.cs@gmail.com>
2024-05-22 22:14:51 +0900

    feat(User): add Exceptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,23 +25,23 @@ export class UsersController {
   }
 
   @Get('/:id')
-  async findUserById(@Param('id') id: string) {
+  findUserById(@Param('id') id: string) {
     return this.usersService.findOneById(parseInt(id));
   }
 
   @Get()
-  async findUsersByEmail(@Query('email') email: string) {
+  findUsersByEmail(@Query('email') email: string) {
     return this.usersService.findByEmail(email);
   }
 
   @Delete('/:id')
-  async removeUser(@Param('id') id: string) {
+  removeUser(@Param('id') id: string) {
     // TODO handle error
     return this.usersService.remove(parseInt(id));
   }
 
   @Patch('/:id')
-  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.update(parseInt(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,8 +14,12 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  findOneById(id: number) {
-    return this.repo.findOne({ where: { id } });
+  async findOneById(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 
   findByEmail(email: string) {
@@ -25,7 +29,7 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOneById(id);
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     // overwrite user with new attrs
     const updatedUser = Object.assign(user, attrs);
@@ -36,7 +40,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOneById(id);
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     // won't use delete method because it doesn't trigger lifecycle hooks
     return this.repo.remove(user);
